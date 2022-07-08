52aef94f4f593500038df6a99dc967ed09dfbd3c
Luc <lucmcgrady@gmail.com>
2022-07-08 23:07:17 +0100

    Added: Title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,18 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import NavBar from 'components/Navbar';
+import Head from 'next/head';
 
 const client = new QueryClient;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
   <QueryClientProvider client={client}>
+    
+    <Head>
+      <title>UCLAN Timetable {"(unofficial)"}</title>
+    </Head>
+    
     <NavBar/>
 
     <div className='content'>
