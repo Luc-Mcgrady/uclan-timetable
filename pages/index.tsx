7ccb90996a7606245bf8a691b9005cdbefdbabbc
Luc <lucmcgrady@gmail.com>
2022-07-08 18:23:12 +0100

    Added: Homepage link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
     <>
+      <Link href={"/timetable"}>WIP heres the timetable</Link>
     </>
   )
 }
