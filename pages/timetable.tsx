908c003809ee3156275b11052c4d8777ae2d00fd
Luc Mcgrady <lucmcgrady@gmail.com>
2022-07-08 19:44:51 +0100

    Added: Custom fetch

--- pages/timetable.tsx
+++ pages/timetable.tsx
@@ -4,21 +4,6 @@ import basicAuth from "lib/auth/basicAuth";
 import { FunctionComponent } from "react";
 import { useQuery } from "react-query";
 
-async function fetchData () {
-	const testConfig : AxiosRequestConfig = {
-		method: "PUT",
-		headers: {
-			Authorization: basicAuth(process.env.NEXT_PUBLIC_EMAIL as string, process.env.NEXT_PUBLIC_PASSWORD as string)
-		},
-		data: {
-			email: process.env.NEXT_PUBLIC_EMAIL as string,
-			date: "2021-10-04"
-		}
-	}
-
-	const response = await axios("/api/timetable", testConfig)
-	return response.data
-}
 
 interface TimetablePageProps {
 	
@@ -26,20 +11,7 @@ interface TimetablePageProps {
 
 const TimetablePage: FunctionComponent<TimetablePageProps> = () => {
 	
-	const {isLoading, isError, error, data} = useQuery(["timetable","test"], fetchData)
-	
-	if (isLoading) {
-		return <>Loading</>
-	}
-	else if (isError) {
-		return <>{(error as any).toString()}</>
-	}
 
-	return ( 
-	<>
-		<Timetable {...data}/>
-	</> 
-	);
 }
  
 export default TimetablePage;
