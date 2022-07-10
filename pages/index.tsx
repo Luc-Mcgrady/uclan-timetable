bc1c094f515b63a85052335aed1837b455e186eb
Luc Mcgrady <lucmcgrady@gmail.com>
2022-07-10 02:06:07 +0100

    Added: Words

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ const Home: NextPage = () => {
         {"I made this to address a series of issues"}
       </p>
       <ul>
-        <li>{"The site asks you to log in through a weird little popup box instead of an actual website."}</li>
+        <li>{"The site asks you to log in through a weird little popup box instead of a part of the actual website."}</li>
         <li>{"The table morphs depending on how many lessons you have making it hard to tell whats where."}</li>
         <li>{"The lesson details are all kind of just in a blob."}</li>
         <li>{"You have to reload the page whenever you want to change the week. (Which wouldent matter if the loadtime wasnt ages)"}</li>
