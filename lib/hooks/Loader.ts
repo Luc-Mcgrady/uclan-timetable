20c4e6fe88590da3938196b0a6d20c51228f1938
Luc <lucmcgrady@gmail.com>
2022-07-14 19:49:31 +0100

    Added: window cache

--- lib/hooks/Loader.ts
+++ lib/hooks/Loader.ts
@@ -1,8 +1,34 @@
 import { useQuery } from "react-query"
 
-export default function useLoader<T>(key: string | string[], dataFetcher: (...args: unknown[])=>Promise<T>) : {status: string, data: undefined} | {data: T, status: undefined} {
+type Key = string | string[]
+
+function stringifyKey(key: Key) {
+
+	if (typeof key === "object") {
+		return key.join("_")
+	}
+	return key
+
+}
+
+export default function useLoader<T>(key: Key, dataFetcher: ()=>Promise<T>) : {status: string, data: undefined} | {data: T, status: undefined} {
 	
-	const {isLoading, isError, isIdle, error, data} = useQuery(key, dataFetcher)
+	const cacheKey = "loader_" + stringifyKey(key)
+
+	const withCache = async () => {
+
+		const cache = window.localStorage.getItem(cacheKey)
+
+		if (!cache) {
+			return await dataFetcher()
+		}
+		else {
+			window.localStorage.setItem(cacheKey, JSON.stringify(data))
+			return JSON.parse(cache)
+		}
+	}
+
+	const {isLoading, isError, isIdle, error, data} = useQuery(key, withCache)
 	
 	if (isLoading) {
 		return {status: "Loading", data: undefined}
