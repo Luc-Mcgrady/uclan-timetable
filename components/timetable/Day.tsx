805d70c769425b5bc59685f751f6aad85869ea2d
Luc <lucmcgrady@gmail.com>
2022-07-15 22:56:25 +0100

    Fix: Lesson conflicts break it

--- components/timetable/Day.tsx
+++ components/timetable/Day.tsx
@@ -1,22 +1,19 @@
 import { DayData } from "lib/site/timetable/parse";
 import { FunctionComponent } from "react";
+import DayHeading from "./DayHeading";
 import Lesson from "./Lesson";
-import style from "./timetable.module.scss";
 
-type DayProps = {
+export type DayProps = {
 	data: DayData
 	row: number
 }
  
-const yOffset = 2
+export const yOffset = 2
 
 const Day: FunctionComponent<DayProps> = ({data, row}) => {
 	return ( 
 	<>
-		<div className={style.header} style={{gridRowStart: row+yOffset}}>
-			<div>{data.day.split(" ")[0]}</div>
-			<div>{data.date}</div>
-		</div>
+		<DayHeading data={data} row={row}/>
 		{data.cells.map(data=><Lesson key={`${data.name}${data.timeframe}`} row={row+yOffset-1} data={data}/>)}
 	</> 
 	);
